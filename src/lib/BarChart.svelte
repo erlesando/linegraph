8ff2bad64a564328d380923b5b1d321645c2feeb
Erle Erlandsen Sandø <ees@norsktest.no>
2025-03-03 12:24:46 +0100

    la inn en liten strek i barchart dersom datapoint=0

--- src/lib/BarChart.svelte
+++ src/lib/BarChart.svelte
@@ -15,12 +15,44 @@
     let showValue = $state("")
 
     import { scale, setYaxis } from '$lib/utils'
+    import { data } from './data';
 </script>
 
 {#each series_i.values as dataPoint, i}
+    {#if dataPoint != 0}
+        <path 
+            class="path" 
+            d = "M {x(i+0.5) - barwidth/2* (numberofseries-1) + barwidth*series_number} {y(0)} L {x(i+0.5) - barwidth/2*(numberofseries-1) + barwidth*series_number} {y(dataPoint)}"
+            stroke-width="{barwidth}" 
+            stroke={series_i.color} 
+            fill="none" 
+            style="stroke-dasharray: {height};  stroke-dashoffset: {height}"
+            aria-label="rectangle"
+            role="presentation"
+            onblur={onBlur} 
+            onfocus={onFocus} 
+            onmouseover={() => hoveredValue = [dataPoint,i]} 
+            onmouseout={() => hoveredValue = [null,i]}
+            />    
+    {:else}    
+        <path 
+        class="path" 
+        d = "M {x(i+0.5) - barwidth/2* (numberofseries-1) + barwidth*series_number -barwidth/2} {y(0)} L {x(i+0.5) - barwidth/2*(numberofseries-1) + barwidth*series_number +barwidth/2} {y(0)}"
+        stroke-width="2" 
+        stroke={series_i.color} 
+        fill="none" 
+        style="stroke-dasharray: {height};  stroke-dashoffset: {height}"
+        aria-label="rectangle"
+        role="presentation"
+        onblur={onBlur} 
+        onfocus={onFocus} 
+        onmouseover={() => hoveredValue = [dataPoint,i]} 
+        onmouseout={() => hoveredValue = [null,i]}
+        />   
+    {/if}
     <path 
         class="path" 
-        d="M {x(i+0.5) - barwidth/2* (numberofseries-1) + barwidth*series_number} {y(0)} L {x(i+0.5) - barwidth/2*(numberofseries-1) + barwidth*series_number} {y(dataPoint)}"
+        d = "M {x(i+0.5) - barwidth/2* (numberofseries-1) + barwidth*series_number} {y(0)} L {x(i+0.5) - barwidth/2*(numberofseries-1) + barwidth*series_number} {y(dataPoint)}"
         stroke-width="{barwidth}" 
         stroke={series_i.color} 
         fill="none" 
@@ -41,12 +73,12 @@
             alignment-baseline="middle"> 
             <tspan 
                 x="{x(hoveredValue[1]+0.5) - barwidth/2*(numberofseries - 1) + barwidth*series_number}" 
-                y="{(hoveredValue[0] < 0 ? (y(hoveredValue[0])+15) : (y(hoveredValue[0])-30))}"
+                y="{(hoveredValue[0] < 0 ? (y(hoveredValue[0])+15) : (y(hoveredValue[0])-20))}"
                 >{series_i.legend} 
             </tspan>
             <tspan 
                 x="{x(hoveredValue[1]+0.5) - barwidth/2*(numberofseries - 1) + barwidth*series_number}" 
-                y="{(hoveredValue[0] < 0 ? (y(hoveredValue[0])+30) : (y(hoveredValue[0])-15))}"
+                y="{(hoveredValue[0] < 0 ? (y(hoveredValue[0])+30) : (y(hoveredValue[0])-5))}"
                 >{hoveredValue[0]}
             </tspan>
             </text>
